f20c74b439ce5c487ff525fa81052140e81430be
Nguyễn Huy <nguyenhuy@MacBook-Air-cua-Nguyen-4.local>
2025-01-21 23:06:29 +0700

    Khởi tạo dự án React từ đầu

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/layout/header";
+import axios from "./util/axios.customize"
+import { useEffect } from "react"
+
+
+function App() {
+
+  useEffect(() => {
+    const fetchHelloWorld = async () => {
+      const res = await axios.get(`/v1/api/`);
+      console.log(">>> check res: ", res)
+    }
+
+    fetchHelloWorld()
+  }, [])
+
+
+
+  return (
+    <div>
+
+      <Header />
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
