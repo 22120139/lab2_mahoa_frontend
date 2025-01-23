ae92dd155e46cac76b39df3cf2e0f6452a14eebb
kuroky3112 <justalittl3@gmail.com>
2025-01-23 22:05:28 +0700

    Sửa lỗi encryptNode thành encryptFile, decryptNode thành decryptFile

--- src/util/encryption.js
+++ src/util/encryption.js
@@ -1,12 +1,12 @@
 import CryptoJS from 'crypto-js';
 
-// Function to encrypt a note
-export const encryptNote = (note, privateKey) => {
-    return CryptoJS.AES.encrypt(note, privateKey).toString();
+// Function to encrypt a file
+export const encryptFile = (fileContent, privateKey) => {
+    return CryptoJS.AES.encrypt(fileContent, privateKey).toString();
 };
 
-// Function to decrypt a note
-export const decryptNote = (encryptedNote, privateKey) => {
-    const bytes = CryptoJS.AES.decrypt(encryptedNote, privateKey);
+// Function to decrypt a file
+export const decryptFile = (encryptedFile, privateKey) => {
+    const bytes = CryptoJS.AES.decrypt(encryptedFile, privateKey);
     return bytes.toString(CryptoJS.enc.Utf8);
 };
